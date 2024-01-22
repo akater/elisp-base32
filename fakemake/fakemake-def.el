b343c69916254fc3f33f11eba7092e5ad5cba272
akater <nuclearspace@gmail.com>
2024-01-22 18:55:14 +0000

    fakemake: Specify author email

--- fakemake/fakemake-def.el
+++ fakemake/fakemake-def.el
@@ -2,7 +2,7 @@
 
 (defconst-with-prefix fakemake
   feature 'base32
-  authors "Dima Akater"
+  authors "Dima Akater" author-email "nuclearspace@gmail.com"
   first-publication-year-as-string "2023"
   org-files-in-order '("base32")
   site-lisp-config-prefix "50"
